6594290c2ad6e0af0d4dc433fbbe8d742f7d744c
Vanessa W <88523307+iNeso1984@users.noreply.github.com>
2023-04-07 15:01:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,39 @@ def CustomChatGPT(user_input):
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
+  
+def get_moderation(question):
+    """
+    Check the question is safe to ask the model
+
+    Parameters:
+        question (str): The question to check
+
+    Returns a list of errors if the question is not safe, otherwise returns None
+    """
+
+    errors = {
+        "hate": "Content that expresses, incites, or promotes hate based on race, gender, ethnicity, religion, nationality, sexual orientation, disability status, or caste.",
+        "hate/threatening": "Hateful content that also includes violence or serious harm towards the targeted group.",
+        "self-harm": "Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders.",
+        "sexual": "Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness).",
+        "sexual/minors": "Sexual content that includes an individual who is under 18 years old.",
+        "violence": "Content that promotes or glorifies violence or celebrates the suffering or humiliation of others.",
+        "violence/graphic": "Violent content that depicts death, violence, or serious physical injury in extreme graphic detail.",
+    }
+    response = openai.Moderation.create(input=question)
+    if response.results[0].flagged:
+        # get the categories that are flagged and generate a message
+        result = [
+            error
+            for category, error in errors.items()
+            if response.results[0].categories[category]
+        ]
+        return result
+    return None
 
 
 
+demo= gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Help Me Find a Good Book?")
 
-iface = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Help Me Find a Good Book?")
-
-iface.launch()
+demo.launch()
